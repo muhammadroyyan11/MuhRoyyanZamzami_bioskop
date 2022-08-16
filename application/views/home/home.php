23f184703c615ac7bd10c101b0ed00c891964e86
muhammadroyyan11 <royyanmz87@gmail.com>
2022-08-17 03:15:08 +0700

    Add bootstrap and home page

---
+++ application/views/home/home.php
@@ -0,0 +1,76 @@
+<!-- Welcome -->
+<section class="jumbotron text-center">
+    <h1>Bioskop Royyan</h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <hr class="my-hr">
+    <p class="lead">It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <a class="btn btn-outline-primary btn-lg" href="#" role="button">Learn more</a>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#FFFFFF" fill-opacity="1" d="M0,64L60,85.3C120,107,240,149,360,176C480,203,600,213,720,197.3C840,181,960,139,1080,138.7C1200,139,1320,181,1380,202.7L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+    </svg>
+</section>
+<!-- end welcome -->
+
+<!-- 6 new film -->
+<section id="newFilm" class="mb-4">
+    <div class="container">
+        <div class="row text-center mb-4">
+            <div class="col-md">
+                <h2>New Film</h2>
+            </div>
+        </div>
+        <div class="row justify-content-center mb-4">
+            <div class="col-md-4 mb-3">
+                <div class="card rapi">
+                    <img src="<?= base_url() ?>assets/img/spiderman.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="text-center">Spider-man</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card rapi">
+                    <img src="<?= base_url() ?>assets/img/spiderman.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="text-center">Spider-man</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card rapi">
+                    <img src="<?= base_url() ?>assets/img/spiderman.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="text-center">Spider-man</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card rapi">
+                    <img src="<?= base_url() ?>assets/img/spiderman.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="text-center">Spider-man</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card rapi">
+                    <img src="<?= base_url() ?>assets/img/spiderman.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="text-center">Spider-man</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-md">
+                <a class="btn btn-outline-primary btn-lg" href="#" role="button">Find more</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- enc new film -->
---
+++ application/views/template.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- link bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link rel="stylesheet" href="<?= base_url() ?>assets/style.css">
+
+    <title>Bioskop | <?= $title ?></title>
+</head>
+
+<body>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="<?= site_url('home') ?>">
+                <b>Bioskop Royyan</b>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#newFilm">New Film</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- end navbar -->
+
+    <?= $contents ?>
+
+    <!-- footer -->
+    <footer class="bg-primary text-white text-center p-3">
+        <p>Copyright &copy; <a href="https://www.instagram.com/mhmmad.royyan/" target="_blank" class="text-white">Muhammad Royyan Zamzami</a></p>
+    </footer>
+    <!-- end footer -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
